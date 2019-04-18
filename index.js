21c26041469ee4d2475d108c7faee9a3022d2123
mixmix <whimful@gmail.com>
2019-04-18 23:08:58 +1200

    tidy up setInterval polling on server.close

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ var Legacy = require('./legacy')
 var mdm = require('mdmanifest')
 var fs = require('fs')
 var Notify = require('pull-notify')
-var pull = require('pull-stream')
 
 module.exports = {
   name: 'replicate',
   version: '2.0.0',
   manifest: mdm.manifest(fs.readFileSync(path.join(__dirname, 'api.md'), 'utf8')),
-  //replicate: replicate,
   init: function (ssbServer, config) {
-    var notify = Notify(), upto
+    var notify = Notify()
     if(!config.replicate || config.replicate.legacy !== false) {
       var replicate = Legacy.call(this, ssbServer, notify, config)
 
@@ -32,4 +30,3 @@ module.exports = {
       }
   }
 }
-
